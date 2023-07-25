773cf35170376571b13f8d1aae554f2dfdf069a2
Antony <140126002+antonypetriumili@users.noreply.github.com>
2023-07-26 01:23:29 +0300

    files upload

---
+++ lib/api_throttling.rb
@@ -0,0 +1,61 @@
+require 'rubygems'
+require 'rack'
+require File.expand_path(File.dirname(__FILE__) + '/handlers/handlers')
+
+class ApiThrottling
+  def initialize(app, options={})
+    @app = app
+    @options = {:requests_per_hour => 60, :cache=>:redis, :auth=>true}.merge(options)
+    @handler = Handlers.cache_handler_for(@options[:cache])
+    raise "Sorry, we couldn't find a handler for the cache you specified: #{@options[:cache]}" unless @handler
+  end
+  
+  def call(env, options={})
+    if @options[:auth]
+      auth = Rack::Auth::Basic::Request.new(env)
+      return auth_required unless auth.provided?
+      return bad_request unless auth.basic?
+    end
+    
+    begin
+	    cache = @handler.new(@options[:cache])
+	    key = generate_key(env, auth)
+	    cache.increment(key)
+	    return over_rate_limit if cache.get(key).to_i > @options[:requests_per_hour]
+	  rescue Errno::ECONNREFUSED
+	    # If Redis-server is not running, instead of throwing an error, we simply do not throttle the API
+	    # It's better if your service is up and running but not throttling API, then to have it throw errors for all users
+	    # Make sure you monitor your redis-server so that it's never down. monit is a great tool for that.
+	  end
+    @app.call(env)
+  end
+  
+  def generate_key(env, auth)
+    return @options[:key].call(env, auth) if @options[:key]
+    auth ? "#{auth.username}_#{Time.now.strftime("%Y-%m-%d-%H")}" : "#{Time.now.strftime("%Y-%m-%d-%H")}"
+  end
+  
+  def bad_request
+    body_text = "Bad Request"
+    [ 400, { 'Content-Type' => 'text/plain', 'Content-Length' => body_text.size.to_s }, [body_text] ]
+  end
+  
+  def auth_required
+    body_text = "Authorization Required"
+    [ 401, { 'Content-Type' => 'text/plain', 'Content-Length' => body_text.size.to_s }, [body_text] ]
+  end
+  
+  def over_rate_limit
+    body_text = "Over Rate Limit"
+    retry_after_in_seconds = (60 - Time.now.min) * 60
+    [ 503, 
+      { 'Content-Type' => 'text/plain', 
+        'Content-Length' => body_text.size.to_s, 
+        'Retry-After' => retry_after_in_seconds.to_s 
+      }, 
+      [body_text]
+    ]
+  end
+end
+
+
---
+++ test/test_api_throttling.rb
@@ -0,0 +1,140 @@
+require 'redis'
+require File.expand_path(File.dirname(__FILE__) + '/test_helper')
+
+#  To Run this test, you need to have the redis-server running.
+#  And you need to have rack-test gem installed: sudo gem install rack-test
+#  For more information on rack-test, visit: http://github.com/brynary/rack-test
+
+class ApiThrottlingTest < Test::Unit::TestCase
+  include Rack::Test::Methods
+  
+  context "using redis" do
+    before do
+      # Delete all the keys for 'joe' in Redis so that every test starts fresh
+      # Having this here also helps as a reminder to start redis-server
+      begin
+  		  r = Redis.new
+  		  r.keys("*").each do |key|
+  		    r.delete key
+  		  end
+  		  
+      rescue Errno::ECONNREFUSED
+        assert false, "You need to start redis-server"
+      end
+    end
+    
+    context "with authentication required" do
+      include BasicTests
+      
+      def app
+        Rack::Builder.new {
+          use ApiThrottling, :requests_per_hour => 3
+          run lambda {|env| [200, {'Content-Type' =>  'text/plain', 'Content-Length' => '12'}, ["Hello World!"] ] }
+        }
+      end
+    
+      should "have Redis as cache handler" do
+        assert_equal "Handlers::RedisHandler", app.to_app.instance_variable_get(:@handler).to_s
+      end
+      
+    end
+    
+    context "without authentication required" do
+      def app
+        Rack::Builder.new {
+          use ApiThrottling, :requests_per_hour => 3, :auth => false
+          run lambda {|env| [200, {'Content-Type' =>  'text/plain', 'Content-Length' => '12'}, ["Hello World!"] ] }
+        }
+      end
+
+      should "not require authorization" do
+        3.times do
+          get '/'
+          assert_equal 200, last_response.status
+        end
+        get '/'
+        assert_equal 503, last_response.status
+      end
+    end
+    
+    context "with rate limit key based on url" do
+      def app
+        Rack::Builder.new {
+          use ApiThrottling, :requests_per_hour => 3, 
+                             :key=>Proc.new{ |env,auth| "#{auth.username}_#{env['PATH_INFO']}_#{Time.now.strftime("%Y-%m-%d-%H")}" }
+          run lambda {|env| [200, {'Content-Type' =>  'text/plain', 'Content-Length' => '12'}, ["Hello World!"] ] }
+        }
+      end
+      
+      should "throttle requests based on the user and url called" do
+        authorize "joe", "secret"
+        3.times do
+          get '/'
+          assert_equal 200, last_response.status
+        end
+        get '/'
+        assert_equal 503, last_response.status
+        
+        3.times do
+          get '/awesome'
+          assert_equal 200, last_response.status
+        end
+        get '/awesome'
+        assert_equal 503, last_response.status
+        
+        authorize "luc", "secret"
+        get '/awesome'
+        assert_equal 200, last_response.status
+        
+        get '/'
+        assert_equal 200, last_response.status
+      end
+    end
+  end
+  
+  context "using active support memory store" do
+    require 'active_support'
+    
+    include BasicTests
+
+    before do
+      @@cache_store = ActiveSupport::Cache.lookup_store(:memory_store)
+    end
+    
+    def app
+      Rack::Builder.new {
+        use ApiThrottling, :requests_per_hour => 3, :cache => @@cache_store
+        run lambda {|env| [200, {'Content-Type' =>  'text/plain', 'Content-Length' => '12'}, ["Hello World!"] ] }
+      }
+    end
+    
+    should "have memcache as cache handler" do
+      assert_equal "Handlers::ActiveSupportCacheStoreHandler", app.to_app.instance_variable_get(:@handler).to_s
+    end
+  end
+    
+    
+  context "using active support memcache store" do
+    require 'active_support'
+    
+    #include BasicTests
+    
+    before do      
+      @@cache_store = ActiveSupport::Cache.lookup_store(:memCache_store)
+      @@cache_store.clear
+    end
+    
+    def app
+      Rack::Builder.new {
+        use ApiThrottling, :requests_per_hour => 3, :cache => @@cache_store
+        run lambda {|env| [200, {'Content-Type' =>  'text/plain', 'Content-Length' => '12'}, ["Hello World!"] ] }
+      }
+    end
+    
+    should "have memcache as cache handler" do
+      assert_equal "Handlers::ActiveSupportCacheStoreHandler", app.to_app.instance_variable_get(:@handler).to_s
+    end
+  end
+
+  
+end
